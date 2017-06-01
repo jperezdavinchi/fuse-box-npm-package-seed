648e4f85f30b795230df66b0c35594ef0e712586
Javier Pérez Ruiz <javier.perez@davinchi.es>
2017-06-01 13:59:38 +0200

    Added scope to the package

--- fuse.js
+++ fuse.js
@@ -3,23 +3,15 @@ const {FuseBox, Sparky} = require("fuse-box");
 const fuse = FuseBox.init({
     homeDir: "./src",
     package: {
-        name: 'fuse-box-npm-package-seed',
+        name: '@f/fuse-box-npm-package-seed',
         main: 'index.ts'
     },
-		globals: {'fuse-box-npm-package-seed': '*'},
+		globals: {'@f/fuse-box-npm-package-seed': '*'},
     output: "dist/$name.js",
 });
 
-fuse.dev();
 
 fuse.bundle("index.js")
-    .instructions("> index.ts")
-    .watch();
+    .instructions("> index.ts");
 
 fuse.run();
-
-Sparky.task("default", () => {
-        return Sparky.watch("./assets", {base: "./src"})
-            .dest("./dist");
-    }
-);
